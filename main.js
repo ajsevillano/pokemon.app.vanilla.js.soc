ce472df2d08160c32de25b379735680a7317ccac
Sevi <correosevi@gmail.com>
2021-11-26 15:04:18 +0000

    get other info from the other endpoind

--- main.js
+++ main.js
@@ -8,23 +8,36 @@ async function getPokemons() {
   // data object with some information and the results
   let data = await response.json();
   let pokemonInfo = data.results;
-  console.log(pokemonInfo);
-  pokemonInfo.map((pokemon) => writeLi(pokemon.name));
+  // console.log(pokemonInfo);
+  pokemonInfo.map((pokemon) => createCard(pokemon.name, pokemon.url));
 }
 
 //Get the <ol>
-const getList = document.querySelector('#pokemon-list');
+const container = document.querySelector('#pokemon-list');
 
 //Create a new Li and change the inner text to the pokemon name
-function writeLi(pokemon) {
-  const createDiv = document.createElement('div');
+async function createCard(pokemon, pokemonDetailsUrl) {
+  //Create the elements
+  const card = document.createElement('div');
   const createH2 = document.createElement('h2');
-const appendedDiv = getList.append(createDiv);
-console.log(appendedDiv);
- appendedDiv.append(createH2);
-return createH2.innerText = pokemon;
-}
+  const p = document.createElement('p');
 
+  let jsonData = await getPokemonInfo(pokemonDetailsUrl);
+
+  //Attach the info
+  createH2.innerText = pokemon;
+  p.innerText = jsonData;
+
+  //Append the elements
+  container.appendChild(card);
+  card.appendChild(createH2);
+  card.appendChild(p);
+}
 
 getPokemons();
 
+async function getPokemonInfo(pokemonDetailsUrl) {
+  let response = await fetch(pokemonDetailsUrl);
+  let data = await response.json();
+  return data.base_experience;
+}
