f10699e9c6211040a1af5b371bd15d65aa6519d8
Sevi <correosevi@gmail.com>
2021-11-26 16:36:15 +0000

    Main screen showing pokemons

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ async function getPokemons() {
   // data object with some information and the results
   let data = await response.json();
   let pokemonInfo = data.results;
-  // console.log(pokemonInfo);
+  console.log(pokemonInfo);
   pokemonInfo.map((pokemon) => createCard(pokemon.name, pokemon.url));
 }
 
@@ -21,23 +21,31 @@ async function createCard(pokemon, pokemonDetailsUrl) {
   const card = document.createElement('div');
   const createH2 = document.createElement('h2');
   const p = document.createElement('p');
+  const img = document.createElement('img');
 
-  let jsonData = await getPokemonInfo(pokemonDetailsUrl);
+  let data = await getPokemonInfo(pokemonDetailsUrl);
+  let imgUrl = data.sprites.other.home.front_default;
+  console.log(data);
 
   //Attach the info
+  card.className = 'card';
   createH2.innerText = pokemon;
-  p.innerText = jsonData;
+  p.innerText = `pokedeck id #${data.id}`;
+  img.src = imgUrl;
+  img.width = 200;
 
   //Append the elements
   container.appendChild(card);
+  card.appendChild(img);
   card.appendChild(createH2);
   card.appendChild(p);
 }
 
+//Run the app
 getPokemons();
 
 async function getPokemonInfo(pokemonDetailsUrl) {
   let response = await fetch(pokemonDetailsUrl);
   let data = await response.json();
-  return data.base_experience;
+  return data;
 }
