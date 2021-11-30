5e9707dfc8b53a1ade93640b61ea75f8d408f28b
Sevi <correosevi@gmail.com>
2021-11-30 19:54:20 +0000

    Extract the limits and offset to objects

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
 //Fetch the information
 //First generation
-const apiUrlFirstGeneration =
-  'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
 
-const apiUrlsecondGen =
-  'https://pokeapi.co/api/v2/pokemon?limit=251&offset=152';
+const firstGenData = { limit: 151, offset: 0 };
+const secondGenData = { limit: 100, offset: 152 };
+const thirdGenData = { limit: 135, offset: 251 };
 
-async function startApp(apiUrl) {
+async function startApp(apiInfo) {
+  let { limit, offset } = apiInfo;
+  let apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+  //Get the data from the api
   data = await getPokemons(apiUrl);
-  await Promise.all(
-    data.map(async (pokemon) => {
-      await createCard(pokemon.name, pokemon.url);
-    })
-  );
-  //Solution 2
-  // data.map(async (pokemon) => createCard(pokemon.name, pokemon.url));
-  // await Promise.all(
-  // Solution 1
-  //   data.map(async (pokemon) => await createCard(pokemon.name, pokemon.url))
-  // );
+  data.map(async (pokemon) => await createCard(pokemon.name, pokemon.url));
 }
 
 //Get the <ol>
 const container = document.querySelector('#pokemon-list');
 
+const gen1Button = document.querySelector('.gen1-button');
 const gen2Button = document.querySelector('.gen2-button');
+const gen3Button = document.querySelector('.gen3-button');
 
-gen2Button.addEventListener('click', () => startApp(apiUrlsecondGen));
+gen1Button.addEventListener('click', () => startApp(firstGenData));
+gen2Button.addEventListener('click', () => startApp(secondGenData));
+gen3Button.addEventListener('click', () => startApp(thirdGenData));
 
 //Create a new Li and change the inner text to the pokemon name
 async function createCard(pokemon, pokemonDetailsUrl) {
@@ -65,7 +61,7 @@ async function createCard(pokemon, pokemonDetailsUrl) {
 }
 
 //Run the app
-startApp(apiUrlFirstGeneration);
+startApp(firstGenData);
 
 async function getPokemons(apiUrl) {
   let response = await fetch(apiUrl);
